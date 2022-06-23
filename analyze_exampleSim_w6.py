69dbc20b9e2a53e81589c502e1cc2a0ca2184b0f
Manuela Runge <manuela.runge@northwestern.edu>
2022-06-23 05:41:34 -0500

    week 6 analyzer

--- analyze_exampleSim_w6.py
+++ analyze_exampleSim_w6.py
@@ -13,9 +13,11 @@ SetupParser.default_block = 'HPC'
 
 user = os.getlogin()  # user initials
 expt_name = f'{user}_FE_2022_example_w6b'  ## change w6a or w6b
-expt_id = '8bb3c038-58da-ec11-a9f8-b88303911bc1'  ## change expt_id
+expt_id = 'e9ffe58f-d1f2-ec11-a9f9-b88303911bc1'  ## change expt_id
 working_dir = os.path.join('simulation_outputs')
 
+serialize_years = 5  # Same as in run_exampleSim_w6a.py
+step = 'burnin'  # 'pickup'  # 'burnin'
 
 def plot_inset_chart(channels_inset_chart, sweep_variables):
     # read in analyzed InsetChart data
@@ -34,62 +36,79 @@ def plot_inset_chart(channels_inset_chart, sweep_variables):
             ax.plot(pdf['date'], pdf[channel], '-', linewidth=0.8, label=p)
         ax.set_title(channel)
         ax.set_ylabel(channel)
-        ax.xaxis.set_major_locator(mdates.MonthLocator(interval=24))
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-    if len(sweep_variables)>1:
+        ax.xaxis.set_major_locator(mdates.MonthLocator(interval=12))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+    if len(sweep_variables) > 1:
         axes[-1].legend(title=sweep_variables)
     fig1.savefig(os.path.join(working_dir, expt_name, 'InsetChart.png'))
 
 
-def plot_summary_report(sweep_variables):
+def plot_summary_report(sweep_variables, selected_age=5):
     # read in analyzed summary reporrt
     channels_summary_report = ['Pop', 'Cases', 'Severe cases', 'PfPR']
     df = pd.read_csv(os.path.join(working_dir, expt_name, 'Agebin_PfPR_ClinicalIncidence_annual.csv'))
     df = df.sort_values(by='agebin')
-    # take mean over all years in report
-    df = df.groupby(['agebin'] + sweep_variables)[channels_summary_report].agg(np.mean).reset_index()
+    df = df.loc[df['agebin'] == selected_age]  # select single agebin
+    ## Take mean of runs
+    df = df.groupby(['year'] + sweep_variables)[channels_summary_report].agg(np.mean).reset_index()
 
     # make summary report plot
     fig2 = plt.figure('Summary Report', figsize=(6, 5))
     fig2.subplots_adjust(right=0.96, left=0.12, hspace=0.55, wspace=0.35, top=0.83, bottom=0.10)
     axes = [fig2.add_subplot(2, 2, x + 1) for x in range(4)]
-    fig2.suptitle(f'Analyzer: AnnualAgebinPfPRAnalyzer')
+    fig2.suptitle(f'Pick up simulation - annual summary report')
 
     for ai, channel in enumerate(channels_summary_report):
         ax = axes[ai]
         ax.set_title(channel)
         ax.set_ylabel(channel)
-        ax.set_ylim(0, max([1, 1.1 * np.max(df[channel])]))
-        ax.set_xlabel('age')
+        ax.set_ylim(0, max([1, np.max(df[channel])]))
+        ax.set_xlabel('year')
 
         for p, pdf in df.groupby(sweep_variables):
-            ax.plot(pdf['agebin'], pdf[channel], '-', linewidth=0.8, label=p)
+            ax.plot(pdf['year'], pdf[channel], '-', linewidth=0.8, label=p)
 
     axes[-1].legend(title=sweep_variables)
-    fig2.savefig(os.path.join(working_dir, expt_name, 'Agebin_PfPR_ClinicalIncidence.png'))
+    fig2.savefig(os.path.join(working_dir, expt_name, 'PfPR_ClinicalIncidence.png'))
 
 
 if __name__ == "__main__":
     SetupParser.init()
 
     """Set sweep_variables and event_list as required depending on experiment"""
-    sweep_variables = ['Run_Number']
     channels_inset_chart = ['Statistical Population', 'New Clinical Cases', 'Adult Vectors', 'Infected']
+    sweep_variables = ['Run_Number']
+    if step == 'pickup':
+        sweep_variables = ['Run_Number', 'cm_cov_U5', 'itn_coverage']
 
-    analyzers = [InsetChartAnalyzer(expt_name=expt_name,
-                                    working_dir=working_dir,
-                                    channels=channels_inset_chart,
-                                    sweep_variables=sweep_variables),
-                 # AnnualAgebinPfPRAnalyzer(expt_name=expt_name,
-                 #                          working_dir=working_dir,
-                 #                          start_year=2022,
-                 #                          sweep_variables=sweep_variables)
-                 ]
+    analyzers_burnin = [InsetChartAnalyzer(expt_name=expt_name,
+                                           working_dir=working_dir,
+                                           channels=channels_inset_chart,
+                                           start_year=2022 - serialize_years,
+                                           sweep_variables=sweep_variables),
+                        ]
 
-    am = AnalyzeManager(expt_id, analyzers=analyzers)
-    am.analyze()
+    analyzers_pickup = [InsetChartAnalyzer(expt_name=expt_name,
+                                           working_dir=working_dir,
+                                           channels=channels_inset_chart,
+                                           start_year=2022,
+                                           sweep_variables=sweep_variables),
+                        AnnualAgebinPfPRAnalyzer(expt_name=expt_name,
+                                                 working_dir=working_dir,
+                                                 start_year=2022,
+                                                 sweep_variables=sweep_variables)
+                        ]
 
-    sweep_vars_for_plotting = sweep_variables  # #[x for x in sweep_variables if x != 'Run_Number']
-    plot_inset_chart(channels_inset_chart, sweep_vars_for_plotting)
-    # plot_summary_report(sweep_vars_for_plotting)
-    plt.show()
+    if step == 'burnin':
+        am = AnalyzeManager(expt_id, analyzers=analyzers_burnin)
+        am.analyze()
+        sweep_vars_for_plotting = ['Run_Number']
+
+    elif step == 'pickup':
+        am = AnalyzeManager(expt_id, analyzers=analyzers_pickup)
+        am.analyze()
+        sweep_vars_for_plotting = [x for x in sweep_variables if x != 'Run_Number']
+        plot_inset_chart(channels_inset_chart, sweep_vars_for_plotting)
+        plot_summary_report(sweep_vars_for_plotting)
+    else:
+        print('Please define step, options are burnin or pickup')
